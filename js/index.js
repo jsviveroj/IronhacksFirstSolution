492bf0710e59ba38ac883544a0521dca2fff880e
Juan <jsviveroj@unal.edu.co>
2018-05-04 12:13:40 -0500

    Jquery added for polygons in maps

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
 
 const NY_LAT_LONG = {lat: 40.7291, lng: -73.9965};
 
-const DISTRICTS_SHAPES_URL = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=json"
+const DISTRICTS_SHAPES_URL = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"
 
 var map;
 
-var districtPolygons = [];
 var districtPoints = [];
-let points = [];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 10,
     center: NY_LAT_LONG
   });
   var marker = new google.maps.Marker({
@@ -25,18 +23,30 @@ function initMap() {
 function loadShapes(URL){
   var data = $.get(URL,function() {})
     .done(function(){
-      console.log(data);
+      data = JSON.parse(data.responseText);
+      console.log(data.features[10].geometry.coordinates);
+      for (var i = 0; i < data.features.length; i++) {
+        var coordinates = data.features[i].geometry.coordinates;
+        var row = [];
+        for (var j = 0; j < coordinates.length; j++) {
+          var piece = coordinates[j];
+          for (var k = 0; k < piece.length; k++) {
+            row.push({lat:piece[k][1],lng:piece[k][0]});
+          }
+        }
+        addPolygons(row);
+      }
+
     })
     .fail(function(error){
       console.log(error);
     })
 }
 
-function addPolygons(){
-  for (var i = 0; i < districtPoints.length; i++) {
+function addPolygons(row){
     var color = getRandomColor();
     var newDistrict = new google.maps.Polygon({
-      paths: districtPoints[i] ,
+      paths: row,
       strokeColor: color,
       strokeOpacity: 0.8,
       strokeWeight: 2,
@@ -44,7 +54,6 @@ function addPolygons(){
       fillOpacity: 0.2
     });
     newDistrict.setMap(map);
-  }
 }
 
 function getRandomColor() {
